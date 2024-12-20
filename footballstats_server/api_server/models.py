21f6b2c7b151a26ce25922243458d92263214fa0
walig-here <daw.walig@gmail.com>
2024-12-20 21:00:01 +0100

    Model

--- footballstats_server/api_server/models.py
+++ footballstats_server/api_server/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User, Permission
 
 # Create your models here.
+class AdminAction(models.Model):
+    action_type: models.ForeignKey = models.ForeignKey(Permission, on_delete=models.CASCADE)
+    user_id: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
+    action_date: models.DateField = models.DateField()
